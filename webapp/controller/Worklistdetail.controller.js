e20d3e7151b5005287f736c48422a4057296a8ef
praveen m <praveengouranga@gmail.com>
2025-09-22 03:34:18 +0000

    lazy loading

--- webapp/controller/Worklistdetail.controller.js
+++ webapp/controller/Worklistdetail.controller.js
@@ -21,6 +21,8 @@ sap.ui.define([
         ProductID: sProductID
     });
 
+    
+
     this.getView().bindElement({
         path: sPath,
         parameters: {
@@ -31,6 +33,7 @@ sap.ui.define([
             dataReceived: () => this.getView().setBusy(false)
         }
     });
+
 },
         
          onInit: function() {
@@ -53,7 +56,7 @@ sap.ui.define([
             this.getView().bindElement({
                 path: sObjectPath,
                 parameters: {
-                    expand: "Category,Supplier"
+                    expand: "Category,Supplier,Order_Details,Order_Details/Order,Order_Details/Order/Employee"
                 },
                 events: {
                     change: this._onBindingChange.bind(this),
@@ -65,6 +68,30 @@ sap.ui.define([
                     }
                 }
             });
+        },
+
+
+            _onBindingChange : function () {
+            var oView = this.getView(),
+                oViewModel = this.getModel("objectView"),
+                oElementBinding = oView.getElementBinding();
+
+            // No data for the binding
+            if (!oElementBinding.getBoundContext()) {
+                this.getRouter().getTargets().display("objectNotFound");
+                return;
+            }
+
+            var oResourceBundle = this.getResourceBundle(),
+                oObject = oView.getBindingContext().getObject(),
+                sObjectId = oObject.ProductName,
+                sObjectName = oObject.Products;
+
+                oViewModel.setProperty("/busy", false);
+                oViewModel.setProperty("/shareSendEmailSubject",
+                    oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+                oViewModel.setProperty("/shareSendEmailMessage",
+                    oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
         }
 
 
@@ -76,16 +103,18 @@ sap.ui.define([
 });
 
 
+
+
+
 // sap.ui.define([
 //     "./BaseController",
 //     "sap/ui/model/json/JSONModel",
-//     "../model/formatter",
-//     "sap/ui/model/Filter",
-//     "sap/ui/model/FilterOperator"
-// ], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+//     "sap/ui/core/routing/History",
+//     "../model/formatter"
+// ], function (BaseController, JSONModel, History, formatter) {
 //     "use strict";
 
-//     return BaseController.extend("ui5.my.products.controller.Worklist", {
+//     return BaseController.extend("ui5.project3.controller.Worklistdetail", {
 
 //         formatter: formatter,
 
@@ -98,98 +127,35 @@ sap.ui.define([
 //          * @public
 //          */
 //         onInit : function () {
-//             var oViewModel;
-
-//             // keeps the search state
-//             this._aTableSearchState = [];
-
-//             // Model used to manipulate control states
-//             oViewModel = new JSONModel({
-//                 worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
-//                 shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
-//                 shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
-//                 tableNoDataText : this.getResourceBundle().getText("tableNoDataText")
-//             });
-//             this.setModel(oViewModel, "worklistView");
-
+//             // Model used to manipulate control states. The chosen values make sure,
+//             // detail page shows busy indication immediately so there is no break in
+//             // between the busy indication for loading the view's meta data
+//             var oViewModel = new JSONModel({
+//                     busy : true,
+//                     delay : 0
+//                 });
+//             this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+//             this.setModel(oViewModel, "objectView");
 //         },
-
 //         /* =========================================================== */
 //         /* event handlers                                              */
 //         /* =========================================================== */
 
-//         /**
-//          * Triggered by the table's 'updateFinished' event: after new table
-//          * data is available, this handler method updates the table counter.
-//          * This should only happen if the update was successful, which is
-//          * why this handler is attached to 'updateFinished' and not to the
-//          * table's list binding's 'dataReceived' method.
-//          * @param {sap.ui.base.Event} oEvent the update finished event
-//          * @public
-//          */
-//         onUpdateFinished : function (oEvent) {
-//             // update the worklist's object counter after the table update
-//             var sTitle,
-//                 oTable = oEvent.getSource(),
-//                 iTotalItems = oEvent.getParameter("total");
-//             // only update the counter if the length is final and
-//             // the table is not empty
-//             if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
-//                 sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
-//             } else {
-//                 sTitle = this.getResourceBundle().getText("worklistTableTitle");
-//             }
-//             this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
-//         },
 
 //         /**
-//          * Event handler when a table item gets pressed
-//          * @param {sap.ui.base.Event} oEvent the table selectionChange event
-//          * @public
-//          */
-//         onPress : function (oEvent) {
-//             // The source is the list item that got pressed
-//             this._showObject(oEvent.getSource());
-//         },
-
-//         /**
-//          * Event handler for navigating back.
-//          * Navigate back in the browser history
+//          * Event handler  for navigating back.
+//          * It there is a history entry we go one step back in the browser history
+//          * If not, it will replace the current entry of the browser history with the worklist route.
 //          * @public
 //          */
 //         onNavBack : function() {
-//             // eslint-disable-next-line sap-no-history-manipulation
-//             history.go(-1);
-//         },
-
-
-//         onSearch : function (oEvent) {
-//             if (oEvent.getParameters().refreshButtonPressed) {
-//                 // Search field's 'refresh' button has been pressed.
-//                 // This is visible if you select any main list item.
-//                 // In this case no new search is triggered, we only
-//                 // refresh the list binding.
-//                 this.onRefresh();
+//             var sPreviousHash = History.getInstance().getPreviousHash();
+//             if (sPreviousHash !== undefined) {
+//                 // eslint-disable-next-line sap-no-history-manipulation
+//                 history.go(-1);
 //             } else {
-//                 var aTableSearchState = [];
-//                 var sQuery = oEvent.getParameter("query");
-
-//                 if (sQuery && sQuery.length > 0) {
-//                     aTableSearchState = [new Filter("ProductName", FilterOperator.Contains, sQuery)];
-//                 }
-//                 this._applySearch(aTableSearchState);
+//                 this.getRouter().navTo("worklist", {}, true);
 //             }
-
-//         },
-
-//         /**
-//          * Event handler for refresh event. Keeps filter, sort
-//          * and group settings and refreshes the list binding.
-//          * @public
-//          */
-//         onRefresh : function () {
-//             var oTable = this.byId("table");
-//             oTable.getBinding("items").refresh();
 //         },
 
 //         /* =========================================================== */
@@ -197,30 +163,64 @@ sap.ui.define([
 //         /* =========================================================== */
 
 //         /**
-//          * Shows the selected item on the object page
-//          * @param {sap.m.ObjectListItem} oItem selected Item
+//          * Binds the view to the object path.
+//          * @function
+//          * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
 //          * @private
 //          */
-//         _showObject : function (oItem) {
-//             this.getRouter().navTo("object", {
-//                 objectId: oItem.getBindingContext().getPath().substring("/Products".length)
-//             });
+//         _onObjectMatched : function (oEvent) {
+//             var sObjectId =  oEvent.getParameter("arguments").objectId;
+//             this._bindView("/Products" + sObjectId);
 //         },
 
 //         /**
-//          * Internal helper method to apply both filter and search state together on the list binding
-//          * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
+//          * Binds the view to the object path.
+//          * @function
+//          * @param {string} sObjectPath path to the object to be bound
 //          * @private
 //          */
-//         _applySearch: function(aTableSearchState) {
-//             var oTable = this.byId("table"),
-//                 oViewModel = this.getModel("worklistView");
-//             oTable.getBinding("items").filter(aTableSearchState, "Application");
-//             // changes the noDataText of the list in case there are no filter results
-//             if (aTableSearchState.length !== 0) {
-//                 oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
-//             }
-//         }
+//         _bindView : function (sObjectPath) {
+//             var oViewModel = this.getModel("objectView");
 
+//             this.getView().bindElement({
+//                 path: sObjectPath,
+//                 parameters: {
+//                     expand: "Category,Supplier,Order_Details,Order_Details/Order,Order_Details/Order/Employee"
+//                 },
+//                 events: {
+//                     change: this._onBindingChange.bind(this),
+//                     dataRequested: function () {
+//                         oViewModel.setProperty("/busy", true);
+//                     },
+//                     dataReceived: function () {
+//                         oViewModel.setProperty("/busy", false);
+//                     }
+//                 }
+//             });
+//         },
+
+//         _onBindingChange : function () {
+//             var oView = this.getView(),
+//                 oViewModel = this.getModel("objectView"),
+//                 oElementBinding = oView.getElementBinding();
+
+//             // No data for the binding
+//             if (!oElementBinding.getBoundContext()) {
+//                 this.getRouter().getTargets().display("objectNotFound");
+//                 return;
+//             }
+
+//             var oResourceBundle = this.getResourceBundle(),
+//                 oObject = oView.getBindingContext().getObject(),
+//                 sObjectId = oObject.ProductName,
+//                 sObjectName = oObject.Products;
+
+//                 oViewModel.setProperty("/busy", false);
+//                 oViewModel.setProperty("/shareSendEmailSubject",
+//                     oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+//                 oViewModel.setProperty("/shareSendEmailMessage",
+//                     oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+//         }
 //     });
+
 // });
