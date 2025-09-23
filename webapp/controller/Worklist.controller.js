74cbf7aff975176df769adf0444c3db12fa07c88
praveen m <praveengouranga@gmail.com>
2025-09-23 12:31:43 +0000

    Using Factory Functions

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -2,11 +2,22 @@ sap.ui.define([
     "./BaseController",
     "sap/ui/model/json/JSONModel",
     "../model/formatter",
+    "sap/ui/model/Sorter",
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/export/Spreadsheet",
-    "sap/ui/export/library"     
-], (BaseController,JSONModel,formatter,Filter,FilterOperator,Spreadsheet,exportLibrary) => {
+    "sap/m/ObjectIdentifier",
+    "sap/m/Text",
+    "sap/m/ObjectStatus",
+    "sap/m/ObjectNumber",
+    "sap/m/ColumnListItem",
+    "sap/m/ObjectListItem",
+    "sap/m/ObjectAttribute",
+    "sap/m/StandardListItem",
+    "sap/ui/core/Icon",
+    "sap/ui/export/library",
+    "sap/m/library"   
+], (BaseController,JSONModel,formatter,Sorter,Filter,FilterOperator,Spreadsheet,ObjectIdentifier,Text,ObjectStatus,ObjectNumber,ColumnListItem,ObjectListItem,ObjectAttribute,StandardListItem,Icon,exportLibrary,mobileLibrary) => {
     "use strict";
     const EdmType = exportLibrary.EdmType;
 
@@ -41,6 +52,11 @@ sap.ui.define([
             
             // Set icon tab counts
             this._setIconFilterCounts();
+
+             // Bind items
+            
+
+
 
         },
 
@@ -137,6 +153,39 @@ sap.ui.define([
 
         })
         },
+
+         _createListItem: function(sId, oContext) {
+        const bDiscontinued = oContext.getProperty("Discontinued")
+
+        if (bDiscontinued) {
+          return new StandardListItem(sId, {
+            icon: "sap-icon://alert",
+            title: "{ProductName}",
+            info: "This item is no longer available",
+            infoState: "Warning"
+          })
+        } else {
+          return new ObjectListItem(sId, {
+            type: mobileLibrary.ListType.Navigation,
+            press: this.onPress.bind(this),
+            title: "{ProductName}",
+            attributes: [
+              new ObjectAttribute({
+                title: "Category",
+                text: "{Category/CategoryName}"
+              })
+            ],
+            firstStatus: new ObjectStatus({
+              text: "{= ${Discontinued} ? ${i18n>discontinued} : ${i18n>inProduction}}",
+              state: "{= ${Discontinued} ? 'Error' : 'Success'}"
+            }),
+            number: "{UnitsInStock}",
+            numberUnit: "In stock"
+          })
+        }
+      },
+
+
 
          _getCoulumnConfig: function() {
             var aColumns = []
